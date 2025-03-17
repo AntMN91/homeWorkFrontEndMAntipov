8bc5ec34492bbebac2936e0a1e3d107a35f07edb
Mikhail Antipov <pelengam@gmail.com>
2025-03-17 22:32:03 +0300

    Инициализация репозитория

---
+++ JS_HomeWork1/index.js
@@ -0,0 +1,43 @@
+console.log("Hello world!");
+
+let Hello = "Привет Мир" ;
+let sum = 123 ;
+let bigsum = 123n ;
+let comparison = 10 > 5 ;
+let value ;
+let age = null;
+let sym = Symbol();
+let symTwo = Symbol();
+
+console.log(Hello,sum,bigsum,comparison,value,age,sym === symTwo);
+console.log(typeof Hello);
+console.log(typeof sum);
+console.log(typeof bigsum);
+console.log(typeof comparison);
+console.log(typeof value);
+console.log(typeof age);
+console.log(typeof sym);
+
+//const lad = null ;
+//console.log(lad);
+
+//lad = 'academy';
+//console.log(lad); выдается ошибка assigenment to constant variable. 
+
+let id = null ;
+console.log(id);
+
+id = 123330;
+console.log(id);
+
+
+
+var user = null;
+console.log(user);
+user = 25 ; 
+console.log (user);
+
+
+
+
+
